94710829bc55cbb9631ce1ce67c5e0cd538a909a
Novi Ratna <noviratna357@gmail.com>
2025-12-26 09:58:38 +0700

    Update Pemesanan 2 - novi

--- src/main/java/org/example/MainFrame.java
+++ src/main/java/org/example/MainFrame.java
@@ -8,22 +8,35 @@ import java.awt.*;
 public class MainFrame extends JFrame {
     private CardLayout cardLayout = new CardLayout();
     private JPanel mainPanel = new JPanel(cardLayout);
+    private Pemesanan pagePemesanan;
+    private Riwayat pageRiwayat;
 
     public MainFrame() {
         setTitle("Coffee Shop UAP 2025");
         setSize(700, 500);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        pagePemesanan = new Pemesanan(this);
+        pageRiwayat = new Riwayat(this);
+
         mainPanel.add(new Login(this), "Login");
         mainPanel.add(new Dashboard(this), "Dashboard");
         mainPanel.add(new DataBarang(this), "DataBarang");
-        mainPanel.add(new Pemesanan(this), "Pemesanan");
-        mainPanel.add(new Riwayat(this), "Riwayat");
+        mainPanel.add(pagePemesanan, "Pemesanan");
+        mainPanel.add(pageRiwayat, "Riwayat");
 
         add(mainPanel);
         setVisible(true);
     }
 
-    public void switchPage(String name) { cardLayout.show(mainPanel, name); }
+    public void switchPage(String name) {
+        if (name.equals("Pemesanan")) {
+            pagePemesanan.refreshComboBox();
+        } else if (name.equals("Riwayat")) {
+            pageRiwayat.refreshTable();
+        }
+        cardLayout.show(mainPanel, name);
+    }
+
     public static void main(String[] args) { new MainFrame(); }
 }
